e58e42448b5f4246b1860500f84fbc9f7c806df3
saladve <sachi.akutsu0111@gmail.com>
2025-12-04 13:08:48 +0900

    feat:Firebase初期設定 - AppDelegate.swiftの追加 - GoogleService.swiftの追加 - WeartherApp.swiftコード追加

---
+++ Wearther/AppDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.swift
+//  Wearther
+//
+//  Created by 阿久津咲千 on 2025/12/04.
+//
+
+import Firebase
+
+func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    FirebaseApp.configure()
+    return true
+}
